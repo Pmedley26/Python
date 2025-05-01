71f219dc50528b9340f864e4af4c148246fa59ae
Patrick Medley <smcinnism@gmail.com>
2025-05-01 12:59:19 -0400

    Add files via upload

---
+++ Test_Exercise2.py
@@ -0,0 +1,18 @@
+first_name = "Patrick"
+last_name = "Medley" 
+message = f"Welcome to Python, {first_name}"
+print(message)
+
+
+next_name = "Cloud Strife"
+print(next_name.title())
+
+
+Quote = 'Martin Luther King once said "That old law of an eye for an eye leaves everyone blind."'
+print(Quote)
+
+
+
+famous_person = "Bruce Lee"
+message = f"{famous_person} once said, 'You want to know the way to win, but never to accept the way to lose. You must free your ambitious mind, and learn the art of dying.'"
+print(message)
