7f0a5eb08af850476167634889b93b5df0ac6c1b
Patrick Medley <smcinnism@gmail.com>
2026-07-16 13:08:23 -0400

    Add files via upload
    Control Flow Practice

---
+++ Control-Flow.py
@@ -0,0 +1,40 @@
+#Request a number from 1 to 9
+n = input("Enter a number between 1 - 9")
+
+if int(n) == 5:
+    print("Bingo!")
+    
+elif int(n) > 4:
+ print("Why so much?")
+
+else:
+ print("Why so little?")
+
+
+# Check if a user-entered password is valid
+
+stored_password = "password123"
+
+# get user input
+
+entered_password = input("Enter your password: ")
+
+# check if passwords match
+
+if entered_password == stored_password:
+  print("Access Granted")
+
+else:
+  print("Access Denied")
+
+
+  # Loops
+  # Used to repeat a block of code multiple times until a specified condition is met for a set number of iterations
+
+
+  # For loop
+  # While loop
+
+
+
+  
